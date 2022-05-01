e938648cda667898f0f598989101737366d5ce5b
GrantGastin <103415118+GrantGastin@users.noreply.github.com>
2022-05-01 13:08:04 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,14 @@ public class Main {
     public static void main(String[] args) {
         int customerAccount = 100;
         int bonusScale = 100;
-        int uploadVolume = 1400;
+        int uploadVolume = 1100;
         int bonus;
 
-        bonus = uploadVolume / bonusScale;
-
+        if (uploadVolume >= 1000){
+            bonus = uploadVolume / bonusScale;
+        } else {
+            bonus = 0;
+        }
         int totalAccount = customerAccount + uploadVolume + bonus;
 
         System.out.println("Вы пополнили счёт на " + uploadVolume + " руб.");
